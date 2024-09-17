e8b4a6b5e0c9dde0d59615651282c2af43accbbb
Austin Larsen <austin.larsen@mailfence.com>
2024-09-17 00:09:20 -0400

    cpp compiler

--- compiler/compiler.cpp
+++ compiler/compiler.cpp
@@ -15,7 +15,21 @@ int main(int argc, char **argv) {
     }
     std::string file_name = argv[argc - 1];
     std::cout << "filename: \"" << file_name << "\""<< std::endl;
-    /* std::ofstream file; */
-    /* file.open("") */
+    std::ifstream file (file_name);
+    std::string line;
+
+    if (!file.is_open()) {
+        std::cout << "could not open file" << std::endl;
+        return 1;
+    }
+
+    while (std::getline(file, line)) {
+        std::regex reg ("^\\s+");
+        if (std::regex_search(line, reg)) {
+            std::cout << line << "\n";
+        }
+    }
+    file.close();
+
     return 0;
 }
