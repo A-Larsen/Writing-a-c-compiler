7819535d753cacab2c6ea1452eda7cd8f5d99683
Austin Larsen <austin.larsen@mailfence.com>
2024-09-19 15:28:42 -0400

    refactoring and commenting

--- compiler/compiler.c
+++ compiler/compiler.c
@@ -31,7 +31,7 @@ const char *token_regexs[TOKEN_COUNT] = {
     [TOKEN_SEMICOLON] = "^;",
     [TOKEN_MULTILINE_COMMENT_START] = "^/\\*",
     // The closing tokens should be last so they are checked last, and most
-    // likely not checked at all in the initial regex check
+    // likely not checked at all in the 1st token check
     [TOKEN_CLOSE_PARANTHESIS] ="^\\)",
     [TOKEN_ClOSE_BRACE] = "^}",
     [TOKEN_MULTILINE_COMMENT_END] = "^\\*/",
@@ -115,33 +115,46 @@ int regex_match(char **found, char *str, const char *regex, bool capture) {
 
 void get_tokens(char *line, uint32_t line_number, const char **regexs,
                 uint8_t regexs_length) {
-    static bool in_comment = false;
+    static uint8_t second_token_check_type = 0;
 
     while (true) {
 REGEX_FOUND:
-        if (in_comment) {
-            while(!regex_match(NULL, line,
-                               token_regexs[TOKEN_MULTILINE_COMMENT_END],
-                               false)) {
-                if (line[0] == '\n') return;
-                int len = strlen(line) - 1;
-                memcpy(line, line + 1, len);
-                line[len] = '\0';
+        // 2nd token check
+        // ===============
+        //     This second token check it to process syntax within an opening
+        //     and closing delimiter
+        switch(second_token_check_type) {
+            case 0: break;
+            case 1: {
+                while(!regex_match(NULL, line,
+                                   token_regexs[TOKEN_MULTILINE_COMMENT_END],
+                                   false)) {
+                    if (line[0] == '\n') return;
+                    int len = strlen(line) - 1;
+                    memcpy(line, line + 1, len);
+                    line[len] = '\0';
+                }
+                second_token_check_type = 0;
+                break;
             }
-            in_comment = false;
         }
 
         if (regex_match(NULL, line, "^\\n*$", false) || line[0] == EOF) break;
         regex_match(NULL, line, "^\\s+", false);
 
-        // initial regex check
+        // 1st token check
+        // ===============
+        //     This fist token check is to check high level tokens and see if a
+        //     second token check is necisarry
         for (uint8_t i = 0; i < regexs_length; ++i) {
             char *match = NULL;
             if (regex_match(&match, line, regexs[i], true)) {
                 printf("%s\n", match);
                 free(match);
+                second_token_check_type = 0;
                 switch(i) {
-                    case TOKEN_MULTILINE_COMMENT_START: in_comment = true;
+                    case TOKEN_MULTILINE_COMMENT_START:
+                        second_token_check_type = 1;
                 }
 
                 goto REGEX_FOUND;
