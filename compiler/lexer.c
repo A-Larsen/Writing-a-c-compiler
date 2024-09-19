75e6ff32dc8ed9956b180d3170e6e020db3bd2c6
Austin Larsen <austin.larsen@mailfence.com>
2024-09-19 18:29:31 -0400

    refactoring | seperate files

--- compiler/lexer.c
+++ compiler/lexer.c
@@ -1,3 +1,20 @@
+/* 
+ * Copyright (C) 2024  Austin Larsen
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 #include "lexer.h"
 
 static bool handle_comment(char *line, const char **token_regexs) {
@@ -43,8 +60,22 @@ REGEX_FOUND:
                 if (handle_comment(line, token_regexs)) return;
                 break;
             }
-            case TOKEN_OPEN_BRACE: lexer->brace_indicator++; break;
-            case TOKEN_ClOSE_BRACE: lexer->brace_indicator--; break;
+            case TOKEN_OPEN_BRACE: {
+                lexer->brace_indicator++;
+                break;
+            }
+            case TOKEN_ClOSE_BRACE: {
+                lexer->brace_indicator--;
+                break;
+            }
+            case TOKEN_OPEN_PARANTHESIS: {
+                lexer->parenthesis_indicator++;
+                break;
+            };
+            case TOKEN_CLOSE_PARANTHESIS: {
+                lexer->parenthesis_indicator--;
+                break;
+            }
         }
 
         second_token_check_type = 0;
@@ -98,6 +129,10 @@ void lexer_run(Lexer *lexer, FILE *fp) {
 
     if (lexer->brace_indicator != 0) {
         fprintf(stderr, "ERROR!\n\tNo Closing brace!");
+        exit(1);
+    }
+    if (lexer->parenthesis_indicator != 0) {
+        fprintf(stderr, "ERROR!\n\tNo Closing paranthesis!");
         exit(1);
     }
 }
